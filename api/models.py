ec0e366582cd616ab2548c3115848785f9d1df6b
Adilet <119230294+siweirdb@users.noreply.github.com>
2024-01-13 21:57:22 +0600

    models Product, Category
    create new models Product and Category

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+    category_id = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
+
